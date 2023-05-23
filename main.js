e2dc3cf8e579e0a548e3933cc0bb1f15b8187d7f
AmaruDeveloper <jvp9830@gmail.com>
2023-05-23 18:52:15 -0500

    update text and css

--- main.js
+++ main.js
@@ -1,45 +1,44 @@
 //alert("Aviso: 'Psyduck solo es un apodo que su madre y hermano usamos con el por cariño'");
-document.querySelector("figure").addEventListener("click", ()=>{
 
-    let sonido = document.getElementById("audio");
-    sonido.play()
+document.querySelector("figure").addEventListener("click", () => {
+  let sonido = document.getElementById("audio");
+  sonido.play();
 
-
-        var imagen = document.getElementById("psyduck1");
-        imagen.classList.add("rotacion");
-        setTimeout(function() {
-          imagen.classList.remove("rotacion");
-        }, 600); // Ajusta el tiempo de espera aquí si deseas una pausa entre las rotaciones
-      
+  var imagen = document.getElementById("psyduck1");
+  imagen.classList.add("rotacion");
+  setTimeout(function () {
+    imagen.classList.remove("rotacion");
+  }, 600); // Ajusta el tiempo de espera aquí si deseas una pausa entre las rotaciones
 });
 
-const gusto1 = document.getElementById("gusto1");
-const gusto2 = document.getElementById("gusto2");
-const gusto3 = document.getElementById("gusto3");
-const gusto4 = document.getElementById("gusto4");
-const gusto5 = document.getElementById("gusto5");
 
-const cargarGusto = (entradas, observador) =>{
+  const gusto1 = document.getElementById("gusto1");
+  const gusto2 = document.getElementById("gusto2");
+  const gusto3 = document.getElementById("gusto3");
+  const gusto4 = document.getElementById("gusto4");
+  const gusto5 = document.getElementById("gusto5");
 
-    entradas.forEach((entrada)=>{
-        if(entrada.isIntersecting){
-            entrada.target.classList.add('visible');
-        }else{
-            //entrada.target.classList.remove('visible');
+  const cargarGusto = (entradas, observador) => {
+    entradas.forEach((entrada) => {
+      if (entrada.isIntersecting) {
+        if (screen.width > 900) {
+        entrada.target.classList.add("visible");
         }
-    })
+      } else {
+        //entrada.target.classList.remove('visible');
+      }
+    });
+  };
 
-}
-
-const observador = new IntersectionObserver(cargarGusto, {
+  const observador = new IntersectionObserver(cargarGusto, {
     root: null,
-    rootMargin: '0px 0px 0px 0px',
-    threshold: 1.0
-});
+    rootMargin: "0px 0px 0px 0px",
+    threshold: 1.0,
+  });
 
-observador.observe(gusto1);
-observador.observe(gusto2);
-observador.observe(gusto3);
-observador.observe(gusto4);
-observador.observe(gusto5);
+  observador.observe(gusto1);
+  observador.observe(gusto2);
+  observador.observe(gusto3);
+  observador.observe(gusto4);
+  observador.observe(gusto5);
 
